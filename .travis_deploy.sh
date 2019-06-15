ea99b16936902bb0968eb4bc5f922c18c76d9056
bartoszek <zero.knowlage.proof@gmail.com>
2019-06-15 10:13:29 +0200

    deploy: rebase before push, skip if package file missing

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -1,3 +1,4 @@
+[ ! -f *pkg.tar.xz ] && exit 0
 eval "$(. PKGBUILD; epoch=${epoch:-0}; declare -p pkgname epoch pkgver pkgrel 2>/dev/null; )"
 file_name="$pkgname-$([ $epoch -gt 0 ] && echo $epoch:)$pkgver-$pkgrel-x86_64.pkg.tar.xz"
 git clone https://$GITHUB_ACCESS_TOKEN@github.com/bartoszek/AUR-repo.git
@@ -6,4 +7,4 @@ cp "$TRAVIS_BUILD_DIR/$file_name" $(pwd)
 git add "$file_name"
 git status
 git commit -a -m "$pkgname $pkgver" -m "build_log: $TRAVIS_BUILD_WEB_URL" 
-git push
+git pull --rebase && git push
