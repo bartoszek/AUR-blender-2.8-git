cd70ad5678e2c098d029321a1cf68083a96a04ee
bartus <szczepaniak.bartek+github@gmail.com>
2019-06-17 19:07:04 +0200

    AUR-repo: disable caching, skip LFS smudge.

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -1,7 +1,7 @@
 [ ! -f *pkg.tar.xz ] && exit 0
 eval "$(. PKGBUILD; epoch=${epoch:-0}; declare -p pkgname epoch pkgver pkgrel 2>/dev/null; )"
 file_name="$pkgname-$([ $epoch -gt 0 ] && echo $epoch:)$pkgver-$pkgrel-x86_64.pkg.tar.xz"
-git clone https://$GITHUB_ACCESS_TOKEN@github.com/bartoszek/AUR-repo.git
+GIT_LFS_SKIP_SMUDGE=1 git clone https://$GITHUB_ACCESS_TOKEN@github.com/bartoszek/AUR-repo.git
 cd AUR-repo || exit 1
 cp "$TRAVIS_BUILD_DIR/$file_name" $(pwd)
 git add "$file_name"
